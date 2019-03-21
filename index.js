c3b5d51d064b94f180f83aa5b9f9f23527397863
SerOinkles <37640844+SerOinkles@users.noreply.github.com>
2019-03-20 20:22:15 -0400

    nothing

---
+++ index.js
@@ -0,0 +1,141 @@
+var Discord = require('discord.io');
+var auth = require('./auth.json');
+Math.random()
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth
+   
+});
+bot.connect();
+bot.on('ready', function (evt) {
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - (' + bot.id + ')');
+    bot.setPresence({ status: 'idle', game: { name: 'with ;help' } });
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == ';') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'oink':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'oink'
+                });
+            break;
+            case 'bestfood':
+            bot.sendMessage({
+                to: channelID,
+                message: 'uncrustable (duh)'
+            }
+
+            );
+            break;
+            case 'Austin':
+            bot.sendMessage({
+                to: channelID,
+                message: 'no'
+            }
+
+            );
+            break;
+            case 'Coleman':
+            bot.sendMessage({
+                to: channelID,
+                message: '-7 blocks a second'
+            }
+
+            );
+            break;
+            case 'John':
+            bot.sendMessage({
+                to: channelID,
+                message: 'brick'
+            }
+
+            );
+            break;
+            case 'help':
+            bot.sendMessage({
+                to: userID,
+                message: 'Hi, I am Oink_Bot. I was created by Mark the Pig. I can execute certain commands that start with a ;. Some of these are *;oink* and *;bestfood*.'
+            });
+            break;
+            case 'AdamC':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Simple logic: You like Macbook, you brick'
+            });
+            break;
+            case 'dieroll1':
+            var number = Math.ceil(Math.random() * 6);
+            bot.sendMessage({
+                to: channelID,
+                message: number
+            });
+            break;
+            case 'dieroll2':
+            for(var i = 0; i < 2; i++) {
+                var number = Math.ceil(Math.random() * 6);
+            bot.sendMessage({ 
+                to: channelID,
+                message: number
+            });
+            }
+            break;
+            case 'd1':
+            var civ = Math.ceil(Math.random() * 36);
+            var civList = ['Venice (so unlucky)','American','Assyrian','Brazillian','Byzantine','Carthaginian','Celtic','Danish','Dutch','Ethiopian','French','German','Greek','Incan','Indian','Indonesian','Japanese','Mayan','Mongolian','Morrocan','Ottoman','Polynesian','Portuguese','Roman','Siamese','Songhai','Swedish','Zulu','Arabian','Austrian','Aztec','Chinese','Korean','Persian','Russian','Shoshone','Spanish',]
+            
+            console.log(civ,civList[civ]);
+            bot.sendMessage({ 
+                to: channelID,
+                message: civList[civ]
+            });
+            
+            break;
+            case 'd2':
+            for(var i = 0; i < 2; i++) {
+                var civ = Math.ceil(Math.random() * 36);
+            var civList = ['Venice (so unlucky)','American','Assyrian','Brazillian','Byzantine','Carthaginian','Celtic','Danish','Dutch','Ethiopian','French','German','Greek','Incan','Indian','Indonesian','Japanese','Mayan','Mongolian','Morrocan','Ottoman','Polynesian','Portuguese','Roman','Siamese','Songhai','Swedish','Zulu','Arabian','Austrian','Aztec','Chinese','Korean','Persian','Russian','Shoshone','Spanish',]
+            
+            console.log(civ,civList[civ]);
+            bot.sendMessage({ 
+                to: channelID,
+                message: civList[civ]
+            });
+            }
+            break;
+            case 'd3':
+            for(var i = 0; i < 3; i++) {
+                var civ = Math.ceil(Math.random() * 36);
+            var civList = ['Venice (so unlucky)','American','Assyrian','Brazillian','Byzantine','Carthaginian','Celtic','Danish','Dutch','Ethiopian','French','German','Greek','Incan','Indian','Indonesian','Japanese','Mayan','Mongolian','Morrocan','Ottoman','Polynesian','Portuguese','Roman','Siamese','Songhai','Swedish','Zulu','Arabian','Austrian','Aztec','Chinese','Korean','Persian','Russian','Shoshone','Spanish',]
+            
+            console.log(civ,civList[civ]);
+            bot.sendMessage({ 
+                to: channelID,
+                message: civList[civ]
+            });
+            }
+            break;
+         }
+     }
+     bot.on('message', function(user, userID, channelID, message, event) {
+        if (message === "bacon") {
+            bot.sendMessage({
+                to: userID,
+                message: ":("
+            });
+        }
+    });
+        
+});
+bot.on('disconnect', function (erMsg, code) {
+    console.log('Bot disconnected from Discord with code ' + code + ' for reason: ' + erMsg);
+});
